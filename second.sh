f8dbc65cba67f7c2fda19e6c96e773efa97611a6
root <root@jenkinslab-lc7m.us-central1-a.c.thinknyxproject.internal>
2019-07-17 09:11:02 +0000

    script added

---
+++ second.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "#####################################"
+echo "######STARTING SCRIPT EXECUTION#####"
+echo "#####################################"
+yum -y install talk
+useradd yogeshraheja
+echo "#####################################"
+echo "#########ENDING SCRIPT EXECUTION#####"
+echo "#####################################"
